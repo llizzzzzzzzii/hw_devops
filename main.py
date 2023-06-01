f91f48bfbcf8f202e92f2239b79fd439fa5187c3
llizzzzzzzzii <liza.zakharowa2003yandex.ru>
2023-06-01 12:38:28 +0300

    update main

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 """
 arithmetic function
 """
-def average(num1,num2,num3):
+
+
+def average(num1, num2, num3):
     """Finding the average"""
-    result=(num1+num2+num3)/3
+    result = (num1 + num2 + num3) / 3
     return result
