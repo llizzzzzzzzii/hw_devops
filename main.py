143cd9301c47b2eb347fc7f49bb9f19b9f259cb0
llizzzzzzzzii <liza.zakharowa2003yandex.ru>
2023-06-01 22:44:37 +0300

    update files

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 """
-arithmetic function
+function about user info
 """
 
+import datetime
 
-def average(num1, num2, num3):
-    """Finding the average"""
-    result = (num1 + num2 + num3) / 3
-    return result
+
+def get_year_of_birth(age):
+    """Get the year"""
+    current_year = datetime.date.today().year
+    return current_year - age
+
+
+def hello_user(name):
+    """Greeting with the user"""
+    return "Hello, " + name + "! "
+
+
+def get_age_user(age):
+    """Getting the user's age"""
+    return "You are " + str(age) + '. '
+
+
+def get_year_user(age):
+    """Getting the user's year of birth"""
+    year_of_birth = get_year_of_birth(age)
+    return "You were born in " + str(year_of_birth) + '.'
+
+
+def get_ifo_about_user(name, age):
+    """Getting full information about the user"""
+    return hello_user(name) + get_age_user(age) + get_year_user(age)
