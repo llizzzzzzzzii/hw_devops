0991ad59a0645d4566e6f818e75780309c8c6fb0
llizzzzzzzzii <liza.zakharowa2003yandex.ru>
2023-06-02 14:33:14 +0300

    update

--- app/main.py
+++ app/main.py
@@ -30,3 +30,5 @@ def get_year_user(age):
 def get_ifo_about_user(name, age):
     """Getting full information about the user"""
     return hello_user(name) + get_age_user(age) + get_year_user(age)
+
+print(get_ifo_about_user('Petya',15))
